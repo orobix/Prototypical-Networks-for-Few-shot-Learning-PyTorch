9ddb9731e42d90688d31da1648911b16a455bd06
dnlcrl <daniele.ciriello@orobix.com>
2018-02-20 12:10:19 +0100

    Adds pydocs

--- src/protonet.py
+++ src/protonet.py
@@ -2,6 +2,9 @@ import torch.nn as nn
 
 
 def conv_block(in_channels, out_channels):
+    '''
+    returns a block conv-bn-relu-pool
+    '''
     return nn.Sequential(
         nn.Conv2d(in_channels, out_channels, 3, padding=1),
         nn.BatchNorm2d(out_channels),
@@ -12,6 +15,8 @@ def conv_block(in_channels, out_channels):
 
 class ProtoNet(nn.Module):
     '''
+    Model as described in the reference paper,
+    source: https://github.com/jakesnell/prototypical-networks/blob/f0c48808e496989d01db59f86d4449d7aee9ab0c/protonets/models/few_shot.py#L62-L84
     '''
     def __init__(self, x_dim=1, hid_dim=64, z_dim=64):
         super(ProtoNet, self).__init__()
