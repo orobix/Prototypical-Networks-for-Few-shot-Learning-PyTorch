56247251036698834edf567556b7ae169bcb9bae
Daniele Ciriello <1212655+dnlcrl@users.noreply.github.com>
2018-03-02 16:36:17 +0100

    Avoids nan sampling

--- src/prototypical_batch_sampler.py
+++ src/prototypical_batch_sampler.py
@@ -6,7 +6,7 @@ import torch
 class PrototypicalBatchSampler(object):
     '''
     PrototypicalBatchSampler: yield a batch of indexes at each iteration.
-    Indexes are calculated by keeping in account 'classes_per_it', 'num_support', 'num_query',
+    Indexes are calculated by keeping in account 'classes_per_it' and 'num_samples',
     In fact at every iteration the batch indexes will refer to  'num_support' + 'num_query' samples
     for 'classes_per_it' random classes.
 
@@ -21,7 +21,7 @@ class PrototypicalBatchSampler(object):
         samples indexes will be infered from this iterable.
         - classes_per_it: number of random classes for each iteration
         - num_samples: number of samples for each iteration for each class (support + query)
-        - iterations:number of iterations (episodes) per epoch
+        - iterations: number of iterations (episodes) per epoch
         '''
         super(PrototypicalBatchSampler, self).__init__()
         self.labels = labels
@@ -35,8 +35,10 @@ class PrototypicalBatchSampler(object):
         self.idxs = range(len(self.labels))
         self.label_tens = np.empty((len(self.classes), max(self.counts)), dtype=int) * np.nan
         self.label_tens = torch.Tensor(self.label_tens)
+        self.label_lens = torch.zeros_like(self.classes)
         for idx, label in enumerate(self.labels):
             self.label_tens[label, np.where(np.isnan(self.label_tens[label]))[0][0]] = idx
+            self.label_lens[label] += 1
 
     def __iter__(self):
         '''
@@ -51,7 +53,7 @@ class PrototypicalBatchSampler(object):
             c_idxs = torch.randperm(len(self.classes))[:cpi]
             for i, c in enumerate(self.classes[c_idxs]):
                 s = slice(i * spc, (i + 1) * spc)
-                sample_idxs = torch.randperm(len(self.label_tens[c]))[:spc]
+                sample_idxs = torch.randperm(self.label_lens[c])[:spc]
                 batch[s] = self.label_tens[c][sample_idxs]
             batch = batch[torch.randperm(len(batch))]
             yield batch
