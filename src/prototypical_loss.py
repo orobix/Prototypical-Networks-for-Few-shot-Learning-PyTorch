2fc8d0c0853de34cd1eecc6e7c48038976455396
dnlcrl <daniele.ciriello@orobix.com>
2018-05-03 17:15:28 +0200

    avoid np usage, bug fixes
    problem here is that for now there still isn't torch.unique for gpu, so if
    we want to compute the loss with data on gpu we get a slowdown, so for now the loss avoid almost completely to put data on cuda. Hopefully we will get a speedup when torch.unique will be available.

--- src/prototypical_loss.py
+++ src/prototypical_loss.py
@@ -53,33 +53,37 @@ def prototypical_loss(input, target, n_support):
     - n_support: number of samples to keep in account when computing
       barycentres, for each one of the current classes
     '''
+    target_cpu = target.to('cpu')
+    input_cpu = input.to('cpu')
+
     def supp_idxs(c):
         # FIXME when torch will support where as np
-        return (target == c).nonzero()[:n_support].squeeze()
+        return target_cpu.eq(c).nonzero()[:n_support].squeeze()
 
     # FIXME when torch.unique will be available on cuda too
-    classes = torch.unique(target.to('cpu')).to(target.device)
+    classes = torch.unique(target_cpu)
     n_classes = len(classes)
     # FIXME when torch will support where as np
-    n_query = len((target == int(classes[0])).nonzero()) - n_support
+    # assuming n_query, n_target constants
+    n_query = target_cpu.eq(classes[0].item()).sum().item() - n_support
 
     support_idxs = list(map(supp_idxs, classes))
 
-    prototypes = torch.stack([input[i].mean(0) for i in support_idxs]).to(target.device)
+    prototypes = torch.stack([input_cpu[i].mean(0) for i in support_idxs])
     # FIXME when torch will support where as np
-    query_idxs = torch.stack(list(map(lambda c: (target == c).nonzero()[n_support:], classes))).view(-1).to(target.device)
+    query_idxs = torch.stack(list(map(lambda c: target_cpu.eq(c).nonzero()[n_support:], classes))).view(-1)
 
-    query_samples = input[query_idxs]
+    query_samples = input.to('cpu')[query_idxs]
     dists = euclidean_dist(query_samples, prototypes)
 
     log_p_y = F.log_softmax(-dists, dim=1).view(n_classes, n_query, -1)
 
-    target_inds = torch.arange(0, n_classes).to(target.device)
+    target_inds = torch.arange(0, n_classes)
     target_inds = target_inds.view(n_classes, 1, 1)
     target_inds = target_inds.expand(n_classes, n_query, 1).long()
 
     loss_val = -log_p_y.gather(2, target_inds).squeeze().view(-1).mean()
     _, y_hat = log_p_y.max(2)
-    acc_val = torch.eq(y_hat, target_inds.squeeze()).float().mean()
+    acc_val = y_hat.eq(target_inds.squeeze()).float().mean()
 
     return loss_val,  acc_val
