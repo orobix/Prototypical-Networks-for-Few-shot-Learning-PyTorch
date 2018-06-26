eaec5a74c6b1117839f4ab776f7aabbf2b808afa
daniele.ciriello <daniele.ciriello@orobix.com>
2018-06-26 11:28:49 +0200

    refactoring

--- src/train.py
+++ src/train.py
@@ -20,54 +20,29 @@ def init_seed(opt):
     torch.cuda.manual_seed(opt.manual_seed)
 
 
-def init_dataset(opt):
-    '''
-    Initialize the datasets, samplers and dataloaders
-    '''
-    train_dataset = OmniglotDataset(mode='train',
-                                    root=opt.dataset_root)
+def init_dataset(opt, mode):
+    return OmniglotDataset(mode=mode, root=opt.dataset_root)
 
-    val_dataset = OmniglotDataset(mode='val',
-                                  root=opt.dataset_root)
 
-    trainval_dataset = OmniglotDataset(mode='trainval',
-                                       root=opt.dataset_root)
+def init_sampler(opt, labels, mode):
+    if 'train' in mode:
+        classes_per_it = opt.classes_per_it_tr
+        num_samples = opt.num_support_tr + opt.num_query_tr
+    else:
+        classes_per_it = opt.classes_per_it_val
+        num_samples = opt.num_support_val + opt.num_query_val
 
-    test_dataset = OmniglotDataset(mode='test',
-                                   root=opt.dataset_root)
+    return PrototypicalBatchSampler(labels=labels,
+                                    classes_per_it=classes_per_it,
+                                    num_samples=num_samples,
+                                    iterations=opt.iterations)
 
-    tr_sampler = PrototypicalBatchSampler(labels=train_dataset.y,
-                                          classes_per_it=opt.classes_per_it_tr,
-                                          num_samples=opt.num_support_tr + opt.num_query_tr,
-                                          iterations=opt.iterations)
 
-    val_sampler = PrototypicalBatchSampler(labels=val_dataset.y,
-                                           classes_per_it=opt.classes_per_it_val,
-                                           num_samples=opt.num_support_val + opt.num_query_val,
-                                           iterations=opt.iterations)
-
-    trainval_sampler = PrototypicalBatchSampler(labels=trainval_dataset.y,
-                                                classes_per_it=opt.classes_per_it_tr,
-                                                num_samples=opt.num_support_tr + opt.num_query_tr,
-                                                iterations=opt.iterations)
-
-    test_sampler = PrototypicalBatchSampler(labels=test_dataset.y,
-                                            classes_per_it=opt.classes_per_it_val,
-                                            num_samples=opt.num_support_val + opt.num_query_val,
-                                            iterations=opt.iterations)
-
-    tr_dataloader = torch.utils.data.DataLoader(train_dataset,
-                                                batch_sampler=tr_sampler)
-
-    val_dataloader = torch.utils.data.DataLoader(val_dataset,
-                                                 batch_sampler=val_sampler)
-
-    trainval_dataloader = torch.utils.data.DataLoader(trainval_dataset,
-                                                      batch_sampler=trainval_sampler)
-
-    test_dataloader = torch.utils.data.DataLoader(test_dataset,
-                                                  batch_sampler=test_sampler)
-    return tr_dataloader, val_dataloader, trainval_dataloader, test_dataloader
+def init_dataloader(opt, mode):
+    dataset = init_dataset(opt, mode)
+    sampler = init_sampler(opt, dataset.y, mode)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_sampler=sampler)
+    return dataloader
 
 
 def init_protonet(opt):
@@ -129,7 +104,8 @@ def train(opt, tr_dataloader, model, optim, lr_scheduler, val_dataloader=None):
             x, y = batch
             x, y = x.to(device), y.to(device)
             model_output = model(x)
-            loss, acc = loss_fn(model_output, target=y, n_support=opt.num_support_tr)
+            loss, acc = loss_fn(model_output, target=y,
+                                n_support=opt.num_support_tr)
             loss.backward()
             optim.step()
             train_loss.append(loss.item())
@@ -146,7 +122,8 @@ def train(opt, tr_dataloader, model, optim, lr_scheduler, val_dataloader=None):
             x, y = batch
             x, y = x.to(device), y.to(device)
             model_output = model(x)
-            loss, acc = loss_fn(model_output, target=y, n_support=opt.num_support_val)
+            loss, acc = loss_fn(model_output, target=y,
+                                n_support=opt.num_support_val)
             val_loss.append(loss.item())
             val_acc.append(acc.item())
         avg_loss = np.mean(val_loss[-opt.iterations:])
@@ -163,7 +140,8 @@ def train(opt, tr_dataloader, model, optim, lr_scheduler, val_dataloader=None):
     torch.save(model.state_dict(), last_model_path)
 
     for name in ['train_loss', 'train_acc', 'val_loss', 'val_acc']:
-        save_list_to_file(os.path.join(opt.experiment_root, name + '.txt'), locals()[name])
+        save_list_to_file(os.path.join(opt.experiment_root,
+                                       name + '.txt'), locals()[name])
 
     return best_state, best_acc, train_loss, train_acc, val_loss, val_acc
 
@@ -180,7 +158,8 @@ def test(opt, test_dataloader, model):
             x, y = batch
             x, y = x.to(device), y.to(device)
             model_output = model(x)
-            _, acc = loss_fn(model_output, target=y, n_support=opt.num_support_tr)
+            _, acc = loss_fn(model_output, target=y,
+                             n_support=opt.num_support_tr)
             avg_acc.append(acc.item())
     avg_acc = np.mean(avg_acc)
     print('Test Acc: {}'.format(avg_acc))
@@ -220,8 +199,12 @@ def main():
         print("WARNING: You have a CUDA device, so you should probably run with --cuda")
 
     init_seed(options)
-    tr_dataloader, val_dataloader, trainval_dataloader, test_dataloader = init_dataset(
-        options)
+
+    tr_dataloader = init_dataloader(options, 'train')
+    val_dataloader = init_dataloader(options, 'val')
+    # trainval_dataloader = init_dataloader(options, 'trainval')
+    test_dataloader = init_dataloader(options, 'test')
+
     model = init_protonet(options)
     optim = init_optim(options, model)
     lr_scheduler = init_lr_scheduler(options, optim)
