75a8cd631acd9308f224e84f155f7ff41fb1cffc
daniele.ciriello <daniele.ciriello@orobix.com>
2018-06-27 09:24:10 +0200

    adds some comment

--- src/prototypical_batch_sampler.py
+++ src/prototypical_batch_sampler.py
@@ -32,18 +32,18 @@ class PrototypicalBatchSampler(object):
         self.classes, self.counts = np.unique(self.labels, return_counts=True)
         self.classes = torch.LongTensor(self.classes)
 
-        # create a matrix of dim: classes X max(elements per class)
+        # create a matrix, indexes, of dim: classes X max(elements per class)
         # fill it with nans
         # for every class c, fill the relative row with the indices samples belonging to c
-        # in indexes_lens we store the number of samples for each class/row
+        # in numel_per_class we store the number of samples for each class/row
         self.idxs = range(len(self.labels))
         self.indexes = np.empty((len(self.classes), max(self.counts)), dtype=int) * np.nan
         self.indexes = torch.Tensor(self.indexes)
-        self.indexes_lens = torch.zeros_like(self.classes)
+        self.numel_per_class = torch.zeros_like(self.classes)
         for idx, label in enumerate(self.labels):
             label_idx = np.argwhere(self.classes == label).item()
             self.indexes[label_idx, np.where(np.isnan(self.indexes[label_idx]))[0][0]] = idx
-            self.indexes_lens[label_idx] += 1
+            self.numel_per_class[label_idx] += 1
 
     def __iter__(self):
         '''
@@ -60,7 +60,7 @@ class PrototypicalBatchSampler(object):
                 s = slice(i * spc, (i + 1) * spc)
                 # FIXME when torch.argwhere will exists
                 label_idx = torch.arange(len(self.classes)).long()[self.classes == c].item()
-                sample_idxs = torch.randperm(self.indexes_lens[label_idx])[:spc]
+                sample_idxs = torch.randperm(self.numel_per_class[label_idx])[:spc]
                 batch[s] = self.indexes[label_idx][sample_idxs]
             batch = batch[torch.randperm(len(batch))]
             yield batch
