9e33e996f974d583b4068e543a1f774bc6cc0297
FrankZappaLevasseur <francois_levasseur@hotmail.com>
2018-11-15 15:31:48 -0500

    code refactor of loss function

--- src/prototypical_loss.py
+++ src/prototypical_loss.py
@@ -51,39 +51,37 @@ def prototypical_loss(model_output, target, n_support):
       barycentres, for each one of the current classes
     '''
 
-    target.to('cpu')
-    model_output.to('cpu')
+    target_cpu = target.to('cpu')
+    model_output_cpu = model_output.to('cpu')
 
-    def get_supports_index(c):
-        # prend les 5 premiers indexes correspondant à la bonne classe
-        return target.eq(c).nonzero()[:n_support].squeeze(1)
+    def get_supports_indexes(c):
+        # takes first 5 indexes of class c to make theme supports
+        return target_cpu.eq(c).nonzero()[:n_support].squeeze(1)
 
-    def get_querys_index(c):
-        # prend le reste de celles qu'on a pogné pour être des supports
-        return target.eq(c).nonzero()[n_support:]
+    def get_queries_indexes(c):
+        return target_cpu.eq(c).nonzero()[n_support:]
 
-    classes = torch.unique(target)
+    classes = torch.unique(target_cpu)
     n_classes = len(classes)
 
-    support_indexes = list(map(get_supports_index, classes))
-    supports = [model_output[idx_list] for idx_list in support_indexes]
-    prototypes = torch.stack([support.mean(0) for support in supports]) # moyenne des supports pour chaque classe
+    support_indexes = list(map(get_supports_indexes, classes))
+    supports = [model_output_cpu[idx_list] for idx_list in support_indexes]
+    prototypes = torch.stack([support.mean(0) for support in supports]) # mean of supports for each class
 
-    queries_indexes = list(map(get_querys_index, classes))
+    queries_indexes = list(map(get_queries_indexes, classes))
     n_query_per_class = len(queries_indexes[0])
-    queries_indexes = torch.stack(queries_indexes).view(-1) # ici les indices sont par groupe de 5, c-a-d par classe
 
-    queries = model_output[queries_indexes] # par groupe de 5
+    queries_indexes = torch.stack(queries_indexes).view(-1)
+    queries = model_output_cpu[queries_indexes] # here, queries are in groups of 5 (by class)
+
     dists = euclidean_dist(queries, prototypes)
+    log_softmax_dists = F.log_softmax(-dists, dim=1).view(n_classes, n_query_per_class, -1)
 
-    log_p_y = F.log_softmax(-dists, dim=1).view(n_classes, n_query_per_class, -1)
+    # mask that allows to keep only the log_softmax of distances between query and support of same class
+    dist_mask = torch.arange(0, n_classes).view(n_classes, 1, 1).expand(n_classes, n_query_per_class, 1).long()
 
-    target_inds = torch.arange(0, n_classes)
-    target_inds = target_inds.view(n_classes, 1, 1)
-    target_inds = target_inds.expand(n_classes, n_query_per_class, 1).long()
-
-    loss_val = -log_p_y.gather(2, target_inds).squeeze().view(-1).mean()
-    _, y_hat = log_p_y.max(2)
-    acc_val = y_hat.eq(target_inds.squeeze()).float().mean()
+    loss_val = -log_softmax_dists.gather(2, dist_mask).squeeze().view(-1).mean()
+    _, y_hat = log_softmax_dists.max(2)
+    acc_val = y_hat.eq(dist_mask.squeeze()).float().mean()
 
     return loss_val,  acc_val
--- src/test/test_prototypical_loss.py
+++ src/test/test_prototypical_loss.py
@@ -1,4 +1,5 @@
 import torch
+import time
 from prototypical_loss import prototypical_loss
 
 
@@ -8,7 +9,12 @@ def test_forKownInput_lossFunctionOutputShouldBeExpectedOne():
     targets = torch.load("./targets.pt")
 
     # act
+    begining_time = time.time()
     loss, acc = prototypical_loss(model_output, target=targets, n_support=5)
+    ending_time = time.time()
+    duration = ending_time - begining_time
+    print("Calculation of loss took {:.3f} seconds".format(duration))
+    print("Previous duration was about {:.3f} seconds".format(0.011900186538696289))
 
     # assert
     expected_loss = 1.8843656778335571
@@ -17,5 +23,4 @@ def test_forKownInput_lossFunctionOutputShouldBeExpectedOne():
     assert abs(expected_acc - acc.item()) < 1e-9
 
 
-if __name__ == '__main__':
-    test_forKownInput_lossFunctionOutputShouldBeExpectedOne()
+test_forKownInput_lossFunctionOutputShouldBeExpectedOne()
